43da727e8b8c5af1817dc2c68e337a8cc2288d66
ToveRumar <45783739+ToveRumar@users.noreply.github.com>
2020-12-09 12:48:19 +0100

    Changed for readability

--- Knapsack.py
+++ Knapsack.py
@@ -4,12 +4,12 @@ class Knapsack:
     def __init__(self,maxWeight):
         self.content=[]
         self.maxWeight=maxWeight
-        self.spaceLeft=maxWeight
+        self.Capacity=maxWeight
 
     def getContent(self):
         return self.content
-    def getSpaceLeft(self):
-        return self.spaceLeft
+    def getCapacity(self):
+        return self.Capacity
 
     def aboveMaxWeight(self):
         weightSum=0
@@ -30,14 +30,14 @@ class Knapsack:
         return weightSum
 
     def putItem(self,item):
-        if item.getWeight()>self.spaceLeft:
+        if item.getWeight()>self.Capacity:
             return False
         else:
             self.content.append(item)
-            self.spaceLeft=self.maxWeight-self.getTotalWeight()
+            self.Capacity=self.maxWeight-self.getTotalWeight()
             return True
     def clear(self):
         self.content.clear()
     
     def __repr__(self):
-        return "My maxWeight is " + str(self.maxWeight) + "\nI have "+ str(self.spaceLeft)+" Space left \nI hold the following items:\n" + str(self.content) +"\n my total value is " + str(self.getTotalValue()) + "\n"
+        return "My maxWeight is " + str(self.maxWeight) + "\nI have "+ str(self.Capacity)+" Space left \nI hold the following items:\n" + str(self.content) +"\n my total value is " + str(self.getTotalValue()) + "\n"
--- main.py
+++ main.py
@@ -10,12 +10,12 @@ allKnapsacks=[Knapsack(6),Knapsack(8),Knapsack(5)]
 def sortByBenefit(Items):                                                            
     return sorted(Items, key=Item.getRelativeBenefit,reverse=True) 
 
-#Returns list of knapsacks soted by spaceleft in ascending order
-def sortKnapsacksBySpaceLeft(knapsacks):
-    return sorted(knapsacks, key=Knapsack.getSpaceLeft) 
+#Returns list of knapsacks soted by Capacity in ascending order
+def sortKnapsacksByCapacity(knapsacks):
+    return sorted(knapsacks, key=Knapsack.getCapacity) 
 
 def putItemInKnapsackLeastSpace(item):
-    knapsacks=sortKnapsacksBySpaceLeft(allKnapsacks)
+    knapsacks=sortKnapsacksByCapacity(allKnapsacks)
     return knapsacks[0].putItem(item)
 
 def putItemInRandomKnapsack(item):
@@ -38,20 +38,20 @@ def greedyRandom():
             sortedItems.remove(item)
 
 def clearAllKnapsacks():
-    for kanpsack in allKnapsacks:
+    for knapsack in allKnapsacks:
         knapsack.clear()
 
+
+
+def printState():
+    for knapsack in allKnapsacks:
+        print(knapsack)
+        print("-----------------------------------")
         
 greedyRandom()
-for knapsack in allKnapsacks:
-    print(knapsack)
-    print("-----------------------------------")
 
 clearAllKnapsacks()
 greedyLeastSpace()
-
-for knapsack in allKnapsacks:
-    print(knapsack)
-    print("-----------------------------------")
+printState()
 
 
